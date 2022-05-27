655ed996f4a0c870a19ad3ddec15b356661f1b83
ammasum <abdullamuhammed76@gmail.com>
2022-05-27 18:42:12 +0600

    export lib

--- lib/flutter_slide.dart
+++ lib/flutter_slide.dart
@@ -1,3 +1,3 @@
 library flutter_slide;
 
-import 'src/slide.dart';
+export 'src/slide.dart';
--- test/flutter_slide_test.dart
+++ test/flutter_slide_test.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slide/src/slide.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_slide/flutter_slide.dart';
